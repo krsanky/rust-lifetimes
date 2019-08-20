d15201d0eda9d85c6ed6315440821b60b2c61f8e
paul wisehart <paul@oldcode.org>
2019-08-19 22:25:20 -0400

    this has struct and impl with an explicit lifetime

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,22 @@
+// The 'a reads `the lifetime a'.
+// https://doc.rust-lang.org/1.9.0/book/lifetimes.html
+//We read &mut i32 as `a mutable reference to an i32' and &'a mut i32
+//as `a mutable reference to an i32 with the lifetime 'a'.
+
+struct Foo<'a> {
+    x: &'a i32,
+}
+
+impl<'a> Foo<'a> {
+    fn x(&self) -> &'a i32 {
+        self.x
+    }
+}
+
 fn main() {
     println!("rust lifetimes");
-}
-// implicit
-fn foo(x: &i32) {}
+    let y = &5; // this is the same as `let _y = 5; let y = &_y;`
+    let f = Foo { x: y };
 
-// explicit
-fn bar<'a>(x: &'a i32) {}
+    println!("x is: {}", f.x());
+}
